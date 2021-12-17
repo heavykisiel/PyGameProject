8e66bf7e284574e2d8ed3a9e403c6845b4cdc3fa
fmpara <95306215+fmpara@users.noreply.github.com>
2021-12-17 09:21:53 +0100

    17.12

--- Client.py
+++ Client.py
@@ -81,7 +81,7 @@ class Player(pygame.sprite.Sprite):
 
         self.rect.x += deltax
         self.rect.y += deltay
-    def changeImage(self):
+    def changeImage(self):  #zmiana avatara playera
         if self.direction == 1 and shoot == False:
             self.image = self.images[4]
         elif self. direction == 1:
@@ -121,7 +121,7 @@ class Player(pygame.sprite.Sprite):
                     bullet = Bullets("bullet",self.rect.centerx, self.rect.centery +(0.5 * self.rect.size[0] * self.direction),2, self.direction, self.speedBullet)
                     bulletGroup.add(bullet)
     #def animation(self):
-    def healthRegenerator(self):
+    def healthRegenerator(self):   # regeneracja zycia jesli moby nie bija
         if self.healthTimer ==0:
             
             if player.health < 100 and enemy.shoot == False:  
@@ -164,10 +164,6 @@ class Player(pygame.sprite.Sprite):
 
 
 
-        
-
-
-
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,imgType, x, y, scale, speed, speedBullet):
@@ -177,7 +173,8 @@ class Enemy(pygame.sprite.Sprite):
         self.imgType = imgType
         self.speed = speed
         self.speedBullet = speedBullet
-        self.direction = 1
+        self.direction = 1 # moze byc 1,-1 lub 0 (0 w przypadku jak jest velocity 2 lub -2)
+        self.velocity = 2 # moze byc 2 lub -2 
         self.flip = False
         image = pygame.image.load(f"img/enemy/{self.imgType}.png")
         self.image = pygame.transform.scale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
@@ -186,16 +183,17 @@ class Enemy(pygame.sprite.Sprite):
         self.healthMin = self.health
         self.rect.center = [x,y]
         self.shootCooldown = 0
-        self.counterOfMoves= 0
+        
         self.lastShot = pygame.time.get_ticks()
         self.randomRun = False 
         self.randomRun2 = False
         self.randomRunCounter = 0
         self.randomRunCounter2 =0
-        self.visibilityLeft = pygame.Rect(0,0,400,20)  # 50  width 50 hight od jakiej widocznosci
-        self.visibilityRight = pygame.Rect(0,0,400,20)
-        self.visibilityBottom = pygame.Rect(0,0,20,400)
-        self.visibilityTop = pygame.Rect(0,0,20,400)
+        self.visibilityLeft = pygame.Rect(0,0,800,20)  # 50  width 50 hight od jakiej widocznosci
+        self.visibilityRight = pygame.Rect(0,0,800,20)
+        self.visibilityBottom = pygame.Rect(0,0,20,800)
+        self.visibilityTop = pygame.Rect(0,0,20,800)
+        
 
     def move(self, aiMovingLeft, aiMovingRight,aiMovingUP, aiMovingDOWN ):
         
@@ -204,18 +202,20 @@ class Enemy(pygame.sprite.Sprite):
         
         if aiMovingLeft:
             deltax = -self.speed
-            self.flip = True
-            #self.direction = -1
+            #self.flip = True
+            self.direction = -1
         if aiMovingRight:
             deltax = self.speed
-            self.flip = False
-            #self.direction = 1
+            #self.flip = False
+            self.direction = 1
 
         if aiMovingDOWN:
             deltay = self.speed
+            self.velocity = 2
             
         if aiMovingUP:
             deltay = -self.speed
+            self.velocity = -2
             
         
 
@@ -229,9 +229,13 @@ class Enemy(pygame.sprite.Sprite):
         #aiMovingRight = False
         
         if self.alive and player.alive:
-            if self.randomRun == False and random.randint(1,1000) == 1:   #random module losowanie random od 1,100
+            if self.randomRun == False and random.randint(1,100) == 1:   #random module losowanie random od 1,100
                 self.randomRun = True
-                self.randomRunCounter = 10
+                self.randomRunCounter = 500
+                self.direction *= -1
+                self.velocity *= -1
+                
+
             
             elif self.visibilityLeft.colliderect(player.rect):
                 self.direction = -1
@@ -240,65 +244,74 @@ class Enemy(pygame.sprite.Sprite):
                 self.direction = 1
                 self.shoot()
             elif self.visibilityTop.colliderect(player.rect):
-                self.direction = -2
+                self.velocity = -2
+                self.direction = 0
                 self.shoot()
             elif self.visibilityBottom.colliderect(player.rect):
-                self.direction = 2
+                self.velocity = 2
+                self.direction = 0
                 self.shoot()
             
+                    
             
             else:
+                    
                 if self.randomRun == False:
                     
                     if self.direction == 1:
                         aiMovingRight = True
-                        aiMovingUP = True
-                    else:
-                        
+                        aiMovingUP = False
+                        aiMovingLeft = False
+                        aiMovingDOWN = False
+                       
+                    
+                    elif self.direction == -1:                        
+                       
                         aiMovingRight = False
                         aiMovingUP= False
+                        aiMovingDOWN = False
+                        aiMovingLeft = True
+                        self.direction == 1
                         
-                    aiMovingLeft = not aiMovingRight
-                    aiMovingDOWN = not aiMovingUP
-                    
-                    self.move(aiMovingLeft, aiMovingRight, aiMovingUP, aiMovingDOWN)
-                    self.counterOfMoves += 1
-                    
-                    if self.counterOfMoves > Size:
-                        self.direction *= -1
-                 
-                        self.counterOfMoves *= -1
-
+                    elif self.direction == 0 and self.velocity == 2:
+                        aiMovingRight = False
+                        aiMovingUP= False
+                        aiMovingDOWN = True
+                        aiMovingLeft = False
+                        self.direction ==1
                         
-                
-
-
+                    elif self.direction == 0 and self.velocity == -2:
+                            
+                        aiMovingRight = False
+                        aiMovingUP= True
+                        aiMovingDOWN = False
+                        aiMovingLeft = False
+                        self.direction ==1
+                        
+                                    
                     
+                    self.move(aiMovingLeft,aiMovingRight,aiMovingUP,aiMovingDOWN)
                     
-                    
-                    
-                
-                    self.visibilityLeft.center = (self.rect.left - 200,self.rect.centery)
-                    self.visibilityRight.center = (self.rect.left +200, self.rect.centery)
-                    self.visibilityTop.center = (self.rect.centerx, self.rect.centery -200)
-                    self.visibilityBottom.center = (self.rect.centerx, self.rect.centery + 200)
+                  
+                    self.visibilityLeft.center = (self.rect.left - 300,self.rect.centery)   # zasieg mobów
+                    self.visibilityRight.center = (self.rect.left +300, self.rect.centery )
+                    self.visibilityTop.center = (self.rect.centerx, self.rect.centery -300)
+                    self.visibilityBottom.center = (self.rect.centerx, self.rect.centery + 300)
                     #pygame.draw.rect(screen,red,self.visibilityRight)
                     #pygame.draw.rect(screen,green,self.visibilityLeft)
                     #pygame.draw.rect(screen,green,self.visibilityTop)
                     #pygame.draw.rect(screen,green,self.visibilityBottom)
 
-                    
-
-
                 else:
                     self.randomRunCounter -= 1
+                    self.randomRunCounter2 -=1  
 
                 
                     if self.randomRunCounter <= 0:
                         self.randomRun = False
+                    elif self.randomRunCounter2 <= 0:
+                        self.randomRun = False
                         
-
-    
     def check_alive(self):
         if self.health <= 0:
             self.health = 0
@@ -312,17 +325,17 @@ class Enemy(pygame.sprite.Sprite):
     def shoot(self):
         
         if self.shootCooldown ==0:
-            self.shootCooldown = 20
+            self.shootCooldown = 30
             if self.direction == 1 or self.direction == -1:
                 bullet = Bullets("bullet",self.rect.centerx + (0.75 * self.rect.size[0] * self.direction),self.rect.centery,2, self.direction,self.speedBullet)
-                bulletGroup.add(bullet)
+                bulletGroupEnemy.add(bullet)
             else:
-                if self.direction == 2: 
-                    bullet = Bullets("bullet",self.rect.centerx, self.rect.centery  + (0.5 * self.rect.size[0] * self.direction) ,2, self.direction, self.speedBullet)
-                    bulletGroup.add(bullet)
+                if self.velocity == 2: 
+                    bullet = Bullets("bullet",self.rect.centerx, self.rect.centery  + (0.5 * self.rect.size[0] * self.velocity) ,2, self.velocity, self.speedBullet)
+                    bulletGroupEnemy.add(bullet)
                 else:
-                    bullet = Bullets("bullet",self.rect.centerx, self.rect.centery +(0.5 * self.rect.size[0] * self.direction),2, self.direction, self.speedBullet)
-                    bulletGroup.add(bullet)
+                    bullet = Bullets("bullet",self.rect.centerx, self.rect.centery +(0.5 * self.rect.size[0] * self.velocity),2, self.velocity, self.speedBullet)
+                    bulletGroupEnemy.add(bullet)
     
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
@@ -339,17 +352,18 @@ class Enemy(pygame.sprite.Sprite):
          #   bullet_group.add(bullet)
          #   self.lastShot = time_now
         
-        if self.rect.left > 0:
+        if self.rect.left > 100:
             self.rect.x -= self.speed
-            #self.direction = -1
-        if (self.rect.right+10) < screenWidth:
-            self.rect.x += self.speed
             #self.direction = 1
             
-        if (self.rect.bottom + 10) > screenHeight:
+        if (self.rect.right+100) < screenWidth:
+            self.rect.x += self.speed
+            #self.direction = -1
+            
+        if (self.rect.bottom + 100) > screenHeight:
             self.rect.y -= self.speed
             self.direction = 1
-        if self.rect.top < 0:
+        if self.rect.top < 10:
             self.rect.y += self.speed
             self.direction = -1
         pygame.draw.rect(screen, red, (self.rect.x,(self.rect.bottom +5),self.rect.width, 5))
@@ -357,6 +371,8 @@ class Enemy(pygame.sprite.Sprite):
             pygame.draw.rect(screen, green, (self.rect.x,(self.rect.bottom +5),int(self.rect.width * (self.healthMin / self.healthMax)), 5))
         if self.health == 0:
             self.kill()
+
+
 class Bullets(pygame.sprite.Sprite):
     def __init__(self,imgType, x, y, scale, direction,speed):
         pygame.sprite.Sprite.__init__(self)
@@ -394,7 +410,7 @@ class Bullets(pygame.sprite.Sprite):
             self.kill()
         
         
-        if pygame.sprite.spritecollide(player, bulletGroup, False):
+        if pygame.sprite.spritecollide(player, bulletGroupEnemy, False):
             if player.alive:
                 player.health -= 2.5
                 player.healthMin -= 2.5
@@ -408,10 +424,9 @@ class Bullets(pygame.sprite.Sprite):
                     
                     print(enemy.health)
                     self.kill()
-                
-
-
+            
 #players_group = pygame.sprite.Group()
+bulletGroupEnemy = pygame.sprite.Group()
 bulletGroup = pygame.sprite.Group()
 enemyGroup = pygame.sprite.Group()
 #player = Player(int(screenWidth) / 2 ,  screenHeight - 100, 10, 10 )
@@ -447,6 +462,8 @@ while run:
     
     bulletGroup.update()
     bulletGroup.draw(screen)
+    bulletGroupEnemy.update()
+    bulletGroupEnemy.draw(screen)
     
     for enemy in enemyGroup:
         enemy.AI()
